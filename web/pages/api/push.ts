203457cdf4f802c3782f1a855f88ba90edc50f0a
Henrique Ferreira <henriquebf@gmail.com>
2021-10-03 20:29:05 +0200

    implement push response

---
+++ web/pages/api/push.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import stravaSettings from '@/services/strava/settings.json';
+import postPushSubscriptions from '@/services/strava/postPushSubscriptions';
+
+// Start push notifications
+const verifyToken = 'm25dYE2zRmBFRifZOC73';
+postPushSubscriptions(verifyToken);
+
+type Data =
+  | {}
+  | {
+      'hub.challenge': string;
+    };
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // Check validation from Strava
+  const stravaEnvSettings = stravaSettings[process.env.NODE_ENV];
+  if (
+    req.query?.['hub.verify_token'] === verifyToken &&
+    req.query?.['hub.mode'] === 'subscribe' &&
+    req.query?.['hub.challenge']
+  ) {
+    console.log(`api/push: responding hub (${req.query?.['hub.challenge']})`);
+    return res
+      .status(200)
+      .json({ 'hub.challenge': req.query?.['hub.challenge'] });
+  }
+
+  // Execute push
+  console.log(
+    `api/push: receiving push for ${req.query?.owner_id}/${req.query?.object_type}`
+  );
+  // WIP
+  res.status(200).json({});
+}
