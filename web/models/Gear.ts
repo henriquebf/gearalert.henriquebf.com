d22512bee164055d1cea9646f0801f0555482252
Henrique Ferreira <henriquebf@gmail.com>
2021-10-03 18:38:54 +0200

    Create Gear model

---
+++ web/models/Gear.ts
@@ -0,0 +1,43 @@
+import { findOne, updateMany, deleteMany } from '@/lib/db';
+
+export interface GearItem {
+  id: string;
+  accountId: string;
+  primary: boolean;
+  name: string;
+  retired: boolean;
+  distance: number;
+  gearType: string;
+  lastChainAt?: number;
+  lastTiresAt?: number;
+  lastBrakePadsAt?: number;
+  lastBrakeCablesAt?: number;
+  lastGearCablesAt?: number;
+}
+
+class Account {
+  _collection: string = 'gears';
+
+  async findOne(filter: Partial<GearItem>): Promise<GearItem> {
+    return findOne(this._collection, filter);
+  }
+
+  async save(item: Partial<GearItem>): Promise<GearItem | undefined> {
+    if (!item.id) throw new Error(`Gear:save: Missing id!`);
+    await updateMany(this._collection, item.id, item);
+    return this.findOne({ id: item.id });
+  }
+
+  async saveAll(items: GearItem[]): Promise<void> {
+    for (const item of items) {
+      console.log('item:', item);
+      await updateMany(this._collection, item.id, item);
+    }
+  }
+
+  async removeByNotIds(ids: string[]): Promise<void> {
+    return deleteMany(this._collection, { id: { $nin: ids } });
+  }
+}
+
+export default new Account();
