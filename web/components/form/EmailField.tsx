b6ede3c5c0f41e3da6100bae4a74ebbbb272d41f
Henrique Ferreira <henriquebf@gmail.com>
2021-10-04 20:07:06 +0200

    validate email address

--- web/components/form/EmailField.tsx
+++ web/components/form/EmailField.tsx
@@ -1,15 +1,24 @@
+import { useState } from 'react';
+import classNames from 'classnames';
 import styles from '@/styles/FormField.module.css';
+import { validateEmailAddress } from '@/helpers/stringHelper';
 
 type Props = { initialValue: string; onChange: (value: string) => void };
 
 const EmailField = ({ initialValue, onChange }: Props) => {
+  const [isValid, setValid] = useState(validateEmailAddress(initialValue));
+
   return (
     <input
       type="email"
       defaultValue={initialValue}
-      className={styles.field}
-      autoFocus
-      onChange={(e) => onChange(e.target.value)}
+      className={classNames(
+        isValid ? [styles.field] : [styles.field, styles.invalid]
+      )}
+      onChange={(e) => {
+        setValid(validateEmailAddress(e.target.value));
+        onChange(e.target.value);
+      }}
     />
   );
 };
