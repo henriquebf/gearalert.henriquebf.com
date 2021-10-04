5d66eb3717d84254a532d75a37ff9eb1c914e665
Henrique Ferreira <henriquebf@gmail.com>
2021-10-04 21:06:27 +0200

    generate email template

--- web/helpers/stringHelper.ts
+++ web/helpers/stringHelper.ts
@@ -1,3 +1,5 @@
+// Validate email address pattern
+// Ref: https://newbedev.com/typescript-email-validation-regex-code-example
 export const validateEmailAddress = (email: string): boolean => {
   const pattern = new RegExp(
     /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
--- web/pages/api/push.ts
+++ web/pages/api/push.ts
@@ -2,9 +2,13 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { verifyToken } from '@/services/strava/postPushSubscriptions';
 import postOAuthToken from 'services/strava/postOAuthToken';
 import getAthlete from '@/services/strava/getAthlete';
-import Account from '@/models/Account';
-import Gear from '@/models/Gear';
-import { generateGearFromAthlete } from '@/helpers/gearHelper';
+import Account, { AccountRecord } from '@/models/Account';
+import Gear, { GearRecord } from '@/models/Gear';
+import {
+  generateGearFromAthlete,
+  populateMaintenanceItems,
+} from '@/helpers/gearHelper';
+import { validateEmailAddress } from '@/helpers/stringHelper';
 
 type Data =
   | {}
@@ -30,7 +34,7 @@ export default async function handler(
 
   // Execute user push
   console.log(`api/push: receiving push for ${req.body.owner_id}...`);
-  let account = await Account.findOne({ stravaId: req.body.owner_id });
+  let account = await Account.findOne({ stravaId: Number(req.body.owner_id) });
   if (account) {
     // Check/Update access token
     if (account.stravaTokenExpiresAt < Date.now() / 1000) {
@@ -54,7 +58,60 @@ export default async function handler(
     await Gear.saveAll(gears);
     await Gear.removeByNotIds(gearIds);
     console.log(`api/push: updated data for ${req.body.owner_id}.`);
+
+    // Check/Send email notifications (async)
+    executeNotifications(account);
+  } else {
+    console.error(`api/push: account not found!`);
   }
 
   res.status(200).json({});
 }
+
+const executeNotifications = async (account: AccountRecord) => {
+  if (!validateEmailAddress(account.email)) return;
+
+  // Find gear that requires notification
+  const gears = await Gear.find({ accountId: account.id });
+  const overdueGear = gears
+    .filter((gear) => gear.isNotificationEnabled)
+    .filter(
+      (gear) =>
+        populateMaintenanceItems(gear).filter(
+          (m) =>
+            m.dueDistance < 0 && // maintenance item is overdue
+            gear.distance !== gear.distanceLastNotification // not yet notified
+        ).length
+    );
+
+  // Prepare data for email template
+  const preTemplate = overdueGear.map((gear) => {
+    const itemsToNotify = populateMaintenanceItems(gear)
+      .map((i) => ({
+        itemName: i.label,
+        dueDistance: i.dueDistance,
+      }))
+      .filter((i) => i.dueDistance < 0);
+    return {
+      gearName: gear.name,
+      itemsToNotify,
+    };
+  });
+
+  // Generate template
+  const template = preTemplate
+    .map((p) => {
+      const itemListStr = p.itemsToNotify
+        .map(
+          (i) =>
+            `<b>${i.itemName}</b> is due for ${Math.abs(
+              Math.floor(i.dueDistance / 1000)
+            )} km`
+        )
+        .join('<br />');
+      return `${p.gearName}:<br />${itemListStr}`;
+    })
+    .join('<br /><br />');
+
+  console.log(template);
+};
